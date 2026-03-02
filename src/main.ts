5d66ccca34e4748111e8ff0e64a792e2d7000b4f
J.H.S_0616 <johyunsoo369@naver.com>
2026-03-02 15:46:56 +0900

    ✨ 코드 구조 개선: 스크립트 경로를 2048 디렉토리로 수정하고, 새 게임 버튼의 불필요한 따옴표 제거

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { CreateBoard } from "./scripts/board";
-import { grid, initializeTiles } from "./scripts/tiles";
+import { CreateBoard } from "./scripts/2048/board";
+import { grid, initializeTiles } from "./scripts/2048/tiles";
 import "./style.css";
-import "./scripts/moveTiles";
-import "./scripts/mergeTiles";
-import { newGame } from "./scripts/newGame";
+import "./scripts/2048/moveTiles";
+import "./scripts/2048/mergeTiles";
+import { newGame } from "./scripts/2048/newGame";
 document.addEventListener("DOMContentLoaded", () => {
   initializeTiles();
   CreateBoard(grid);
