625980d022746523d79e9113aa1bc22e4caa49a0
J.H.S_0616 <johyunsoo369@naver.com>
2026-03-08 00:41:58 +0900

    ✨ 모바일 관련 핫스팟 생성 코드 주석 처리: 모바일 전용 핫스팟 로직 비활성화

--- src/scripts/roomEscape/room1.ts
+++ src/scripts/roomEscape/room1.ts
@@ -19,21 +19,22 @@ export function createRoom1() {
 
   const isMobile = window.innerWidth <= 768;
 
-  if (isMobile) {
-    createHotSpot(30, 55, 30, 20, () => {
-      RoomTextChange("각종 메스들과 의료 약품이 보이는 책상이다.");
-    });
-    createHotSpot(90, 57, 28, 18, () => {
-      RoomTextChange(
-        "의료 일지와 환자 기록이 보이는 책상이다. 서랍속에는 환자 기록이 더 많이 보인다. ",
-      );
-    });
-    createHotSpot(52, 80, 25, 18, () => {
-      RoomTextChange(
-        "병원에서 일하는 의사의 메달이 바닥에 떨어져 있다. 메달에는 이름과 분야가 적혀있다. 이름은 '박준석'이고, 분야는 '흉부외과'이다. ",
-      );
-    });
-  } else {
+  // if (isMobile) {
+  //   createHotSpot(30, 55, 30, 20, () => {
+  //     RoomTextChange("각종 메스들과 의료 약품이 보이는 책상이다.");
+  //   });
+  //   createHotSpot(90, 57, 28, 18, () => {
+  //     RoomTextChange(
+  //       "의료 일지와 환자 기록이 보이는 책상이다. 서랍속에는 환자 기록이 더 많이 보인다. ",
+  //     );
+  //   });
+  //   createHotSpot(52, 80, 25, 18, () => {
+  //     RoomTextChange(
+  //       "병원에서 일하는 의사의 메달이 바닥에 떨어져 있다. 메달에는 이름과 분야가 적혀있다. 이름은 '박준석'이고, 분야는 '흉부외과'이다. ",
+  //     );
+  //   });
+  // } else
+  if (!isMobile) {
     createHotSpot(20, 60, 20, 18, () => {
       RoomTextChange("각종 메스들과 의료 약품이 보이는 책상이다.");
       createProviso("3번 단서");
