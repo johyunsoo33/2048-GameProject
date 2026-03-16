e1519ee2553f031d3577fc4c77603998c106e749
J.H.S_0616 <johyunsoo369@naver.com>
2026-03-16 15:00:32 +0900

    ✨ 단서 텍스트 및 프로비소 데이터 업데이트: proviso.json 수정 및 room1.ts, room2.ts에서 단서 관련 로직 개선

--- src/scripts/roomEscape/room1.ts
+++ src/scripts/roomEscape/room1.ts
@@ -1,9 +1,14 @@
 import { createHotSpot } from "./createHotSpot";
 import { createItem } from "./createItem";
 import { NextStage } from "./NextStage";
-import { createProviso, areAllCluesCollected, isClueCollected } from "./proviso";
+import {
+  createProviso,
+  areAllCluesCollected,
+  isClueCollected,
+} from "./proviso";
 import { RoomTextChange } from "./RoomTextChange";
 import itemData from "../../data/RoomEscapeText/item.json";
+import provisoData from "../../data/RoomEscapeText/proviso.json";
 
 export function createRoom1() {
   const roomBgImage = document.getElementsByClassName(
@@ -38,43 +43,37 @@ export function createRoom1() {
   if (!isMobile) {
     createHotSpot(20, 60, 20, 18, () => {
       if (isClueCollected("3번 단서")) {
-        RoomTextChange("이미 조사한 책상이다. 메스와 의료 약품 외엔 특별한 게 없었다.");
+        RoomTextChange(provisoData["3번 단서"][0].text_repeat);
         return;
       }
-      RoomTextChange("각종 메스들과 의료 약품이 보이는 책상이다.");
+      RoomTextChange(provisoData["3번 단서"][0].text_first);
       createProviso("3번 단서");
       createItem(itemData["1번 증거"][0].item);
     });
     createHotSpot(75, 60, 20, 18, () => {
       if (isClueCollected("1번 단서")) {
-        RoomTextChange("이미 살펴본 책상이다. 환자 기록은 이미 확인했다.");
+        RoomTextChange(provisoData["1번 단서"][0].text_repeat);
         return;
       }
-      RoomTextChange(
-        "의료 일지와 환자 기록이 보이는 책상이다. 서랍속에는 환자 기록이 더 많이 보인다. ",
-      );
+      RoomTextChange(provisoData["1번 단서"][0].text_first);
       createProviso("1번 단서");
       createItem(itemData["2번 증거"][0].item);
     });
     createHotSpot(53, 78, 10, 18, () => {
       if (isClueCollected("2번 단서")) {
-        RoomTextChange("이미 확인한 메달이다. 박준석, 흉부외과.");
+        RoomTextChange(provisoData["2번 단서"][0].text_repeat);
         return;
       }
-      RoomTextChange(
-        "병원에서 일하는 의사의 메달이 바닥에 떨어져 있다. 메달에는 이름과 분야가 적혀있다. 이름은 '박준석'이고, 분야는 '흉부외과'이다. ",
-      );
+      RoomTextChange(provisoData["2번 단서"][0].text_first);
       createProviso("2번 단서");
       createItem(itemData["3번 증거"][0].item);
     });
     createHotSpot(63, 25, 10, 48, () => {
       if (!areAllCluesCollected(["1번 단서", "2번 단서", "3번 단서"])) {
-        RoomTextChange("아직 단서를 모두 수집하지 못했다. 더 살펴봐야 할 것 같다.");
+        RoomTextChange(provisoData["수술실 문"][0].text_fail);
         return;
       }
-      RoomTextChange(
-        "수술실로 가는 문이 있다. 들어가보자 더 많은 단서가 있을지도 모르니까.",
-      );
+      RoomTextChange(provisoData["수술실 문"][0].text_success);
       NextStage();
     });
   }
--- src/scripts/roomEscape/room2.ts
+++ src/scripts/roomEscape/room2.ts
@@ -1,3 +1,14 @@
+import { createHotSpot } from "./createHotSpot";
+import { createItem } from "./createItem";
+import {
+  areAllCluesCollected,
+  createProviso,
+  isClueCollected,
+} from "./proviso";
+import { RoomTextChange } from "./RoomTextChange";
+import itemData from "../../data/RoomEscapeText/item.json";
+import { NextStage } from "./NextStage";
+
 export function createRoom2() {
   const roomBgImage = document.getElementsByClassName(
     "RoomBgImage",
@@ -9,5 +20,51 @@ export function createRoom2() {
   ) as HTMLSourceElement;
   if (pictureSource) {
     pictureSource.srcset = "/room2-mobile.png";
+  }
+
+  const isMobile = window.innerWidth <= 768;
+
+  if (!isMobile) {
+    createHotSpot(20, 60, 20, 18, () => {
+      if (isClueCollected("3번 단서")) {
+        RoomTextChange(
+          "이미 조사한 책상이다. 메스와 의료 약품 외엔 특별한 게 없었다.",
+        );
+        return;
+      }
+      RoomTextChange("각종 메스들과 의료 약품이 보이는 책상이다.");
+      createProviso("3번 단서");
+      createItem(itemData["1번 증거"][0].item);
+    });
+    createHotSpot(75, 60, 20, 18, () => {
+      if (isClueCollected("1번 단서")) {
+        RoomTextChange("이미 살펴본 책상이다. 환자 기록은 이미 확인했다.");
+        return;
+      }
+      RoomTextChange(
+        "의료 일지와 환자 기록이 보이는 책상이다. 서랍속에는 환자 기록이 더 많이 보인다. ",
+      );
+      createProviso("1번 단서");
+      createItem(itemData["2번 증거"][0].item);
+    });
+    createHotSpot(53, 78, 10, 18, () => {
+      if (isClueCollected("")) {
+        RoomTextChange("이미 확인한 메달이다. 박준석, 흉부외과.");
+        return;
+      }
+      RoomTextChange(
+        "병원에서 일하는 의사의 메달이 바닥에 떨어져 있다. 메달에는 이름과 분야가 적혀있다. 이름은 '박준석'이고, 분야는 '흉부외과'이다. ",
+      );
+      createProviso("2번 단서");
+      createItem(itemData["3번 증거"][0].item);
+    });
+    createHotSpot(63, 25, 10, 48, () => {
+      if (isClueCollected("")) {
+        RoomTextChange("이미 확인한 의약품들이다.");
+        return;
+      }
+      RoomTextChange("수술실에서 사용한 의약품같다 확인해보자");
+      NextStage();
+    });
   }
 }
