6678265079f2d5de7be3839e085319b00299aaf1
J.H.S_0616 <johyunsoo369@naver.com>
2026-03-16 15:20:15 +0900

    ✨ 단서 텍스트 업데이트: room1.ts에서 단서 관련 로직 주석 해제 및 provisoData 사용

--- src/scripts/roomEscape/room1.ts
+++ src/scripts/roomEscape/room1.ts
@@ -27,17 +27,13 @@ export function createRoom1() {
 
   // if (isMobile) {
   //   createHotSpot(30, 55, 30, 20, () => {
-  //     RoomTextChange("각종 메스들과 의료 약품이 보이는 책상이다.");
+  //     RoomTextChange(provisoData["1번 단서"][0].text_first);
   //   });
   //   createHotSpot(90, 57, 28, 18, () => {
-  //     RoomTextChange(
-  //       "의료 일지와 환자 기록이 보이는 책상이다. 서랍속에는 환자 기록이 더 많이 보인다. ",
-  //     );
+  //     RoomTextChange(provisoData["3번 단서"][0].text_first);
   //   });
   //   createHotSpot(52, 80, 25, 18, () => {
-  //     RoomTextChange(
-  //       "병원에서 일하는 의사의 메달이 바닥에 떨어져 있다. 메달에는 이름과 분야가 적혀있다. 이름은 '박준석'이고, 분야는 '흉부외과'이다. ",
-  //     );
+  //     RoomTextChange(provisoData["2번 단서"][0].text_first);
   //   });
   // } else
   if (!isMobile) {
